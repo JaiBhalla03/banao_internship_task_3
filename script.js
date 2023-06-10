f0b69fb2f1970db5b237ab897785874ef4157b6f
jai <jai.bhalla2021@vitstudent.ac.in>
2023-06-10 19:57:30 +0530

    Adding the autoplay in the carousel

--- script.js
+++ script.js
@@ -118,6 +118,32 @@ const glider = new Glider(document.querySelector('.glider'), {
     ],
 });
 
+function sliderAuto(slider, miliseconds) {
+    const slidesCount = slider.track.childElementCount;
+    let slideTimeout = null;
+    let nextIndex = 1;
+
+    function slide () {
+        slideTimeout = setTimeout(
+            function () {
+                if (nextIndex >= slidesCount ) {
+                    nextIndex = 0;
+                }
+                slider.scrollItem(nextIndex++);
+            },
+            miliseconds
+        );
+    }
+
+    slider.ele.addEventListener('glider-animated', function() {
+        window.clearInterval(slideTimeout);
+        slide();
+    });
+
+    slide();
+}
+
+sliderAuto(glider, 2000)
 
 //understanding how to figure out the scroll for desktop
 window.addEventListener('scroll', function() {
